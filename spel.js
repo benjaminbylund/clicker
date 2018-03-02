708845e609f9fbb8d02c003824d6b3cde5364993
benjaminbylund <31540367+benjaminbylund@users.noreply.github.com>
2018-03-02 14:55:14 +0100

    Add files via upload

--- spel.js
+++ spel.js
@@ -1,9 +1,10 @@
 /* element för manipulation */
 var button = document.getElementById("clickerbutton");
-var upgradeButtonOne = document.getElementById("upgradeOne");
 var scoreDiv = document.getElementById("score");
 var powerText = document.getElementById("powerText");
+var upgradeButtonOne = document.getElementById("upgradeOne");
 var upgradeButtonTwo = document.getElementById("upgradeTwo");
+var upgradeButtonThree = document.getElementById("upgradeThree")
 
 /* Skapa ett nytt element för poängen */
 var scoreText = document.createElement("p");
@@ -11,66 +12,81 @@ var scoreText = document.createElement("p");
 /* spelvariabler */
 var clickValue = 1;
 var bank = 0;
-var upgradeOneCost = 15;
 var lejonClicks = 0;
-var upgradeCostTwo = 30;
+var upgradeOneCost = 15;
+var upgradeTwoCost = 30;
+var upgradeThreeCost = 45;
 var zebra = null;
 var zebraTimer = 0;
 
 /* startvärden */
 scoreText.textContent = "Points: 0";
 upgradeButtonOne.textContent = "Lejon: " + upgradeOneCost;
-upgradeButtonTwo.textContent = "Zebra: " + upgradeCostTwo;
+upgradeButtonTwo.textContent = "Kors: " + upgradeTwoCost;
+upgradeButtonThree.textContent = "Körsbär: " + upgradeThreeCost;
 
 /* click event + logic */
 button.addEventListener("click", function() {
-	// kontrollera om vi har ett lejon aktivt, annars återställ clickValue
-	if (lejonClicks > 0) {
-		lejonClicks--;
-	} else if (lejonClicks == 0) {
-		clickValue = 1;		
-	}
 	bank += clickValue; // lägg till värdet vid click
 	scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
 }, true);
 
 /* kod för första upgraderingen med räknare */
-upgradeButtonOne.addEventListener("click", function() {
-	if (bank >= upgradeCostTwo && zebraTimer == 0) {
-		bank -= upgradeCostTwo;
-		zebraTimer += 10;
-		powerText.textContent = "Köpte zebra";
+upgradeButtonTwo.addEventListener("click", function() {
+	if (bank >= upgradeTwoCost) {
+		bank -= upgradeTwoCost;
+		clickValue +=1;
+		powerText.textContent = "Köpte Kors";
+		upgradeTwoCost*= 2;
 
-		// Lägg till setInterval med en funktion som laddas varje sekund
-		zebra = setInterval(function() {
-			bank += 10;
-			scoreText.textContent = "Points: " + bank;
-			zebraTimer--;
-
-			if (zebraTimer == 0) {
-				powerText.textContent = "Slut på zebra";
-				clearInterval(zebra);  // kalla på clearInterval för att rensa setInterval
-			}
+		upgradeButtonTwo.textContent = "Kors " + upgradeTwoCost;
+		
+		zebra = setInterval(function(){
+			bank +=2;
+			scoreText.textContent = "Points " + bank;
 		}, 1000);
-	} else if (zebraTimer > 0) {
-		powerText.textContent = "Du har redan zebra";
-	} else {
-		powerText.textContent = "Du har inte råd med zebra";
+
+	}else {
+		powerText.textContent = "Du har inte råd med Kors";
 	}
 }, true);
 
 // knapp och kod för andra upgraderingen
-upgradeButtonTwo.addEventListener("click", function() {
+upgradeButtonOne.addEventListener("click", function() {
 	if (bank >= upgradeOneCost) {
 		clickValue *= 2;
 		bank -= upgradeOneCost;
 		upgradeOneCost *= 2;
-		lejonClicks += 10;
 		upgradeButtonOne.textContent = "Lejon: " + upgradeOneCost;
 		powerText.textContent = "Köpte lejon";
 		scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
+		
+		zebra = setInterval(function(){
+			bank +=2;
+			scoreText.textContent = "Points " + bank;
+		}, 1000);
+
 	} else {
 		powerText.textContent = "Du har inte råd med lejon";
+	}
+}, true);
+
+upgradeButtonThree.addEventListener("click", function() {
+	if (bank >= upgradeThreeCost) {
+		clickValue *= 2;
+		bank -= upgradeThreeCost;
+		upgradeThreeCost *= 2;
+		upgradeButtonThree.textContent = "körsbär: " + upgradeThreeCost;
+		powerText.textContent = "Köpte körsbär";
+		scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
+		
+		zebra = setInterval(function(){
+			bank +=2;
+			scoreText.textContent = "Points " + bank;
+		}, 1000);
+
+	} else {
+		powerText.textContent = "Du har inte råd med körsbär";
 	}
 }, true);
 
