8fe8f72bb80f065400e6627f87fa7e4984b85128
benjaminbylund <31540367+benjaminbylund@users.noreply.github.com>
2018-05-05 15:54:10 +0200

    Add files via upload
    Skapade 3 uppgraderingar till och centrerade dom. 
    2/3 av de nya uppgraderingarna har en ikon.

--- spel.js
+++ spel.js
@@ -1,13 +1,22 @@
+
 /* element för manipulation */
 var button = document.getElementById("clickerbutton");
 var scoreDiv = document.getElementById("score");
 var powerText = document.getElementById("powerText");
+
 var upgradeButtonOne = document.getElementById("upgrade1");
 var upgradeButtonTwo = document.getElementById("upgrade2");
-var upgradeButtonThree = document.getElementById("upgrade3")
+var upgradeButtonThree = document.getElementById("upgrade3");
+var upgradeButtonFour = document.getElementById("upgrade4");
+var upgradeButtonFive = document.getElementById("upgrade5");
+var upgradeButtonSix = document.getElementById("upgrade6");
+
 var lejonCounter = document.getElementById("lejonCounter");
 var korsCounter = document.getElementById("korsCounter");
 var körsbärCounter = document.getElementById("körsbärCounter");
+var grönDiamantCounter = document.getElementById("grönDiamantCounter");
+var lilaDiamantCounter = document.getElementById("lilaDiamantCounter");
+var tärningarCounter = document.getElementById("tärningarCounter");
 
 /* Skapa ett nytt element för poängen */
 var scoreText = document.createElement("p");
@@ -19,20 +28,36 @@ var lejonClicks = 0;
 var upgradeOneCost = 15;
 var upgradeTwoCost = 30;
 var upgradeThreeCost = 45;
+var upgradeFourCost = 60;
+var upgradeFiveCost = 80;
+var upgradeSixCost = 100;
+
 var lejonCount = 0;
 var korsCount = 0;
 var körsbärCount = 0;
+var grönDiamantCount = 0;
+var lilaDiamantCount = 0;
+var tärningCount = 0;
+
 var zebra = null;
 var zebraTimer = 0;
 
 /* startvärden */
 scoreText.textContent = "Points: 0";
+
 lejonCounter.textContent = "Lejon: 0";
 korsCounter.textContent = "Kors: 0";
 körsbärCounter.textContent = "Körsbär: 0";
+grönDiamantCounter.textContent = "Diamant(grön): 0";
+lilaDiamantCounter.textContent = "Diamant(lila): 0";
+tärningarCounter.textContent = "Tärningar: 0";
+
 upgradeButtonOne.textContent = "Lejon: " + upgradeOneCost;
 upgradeButtonTwo.textContent = "Kors: " + upgradeTwoCost;
 upgradeButtonThree.textContent = "Körsbär: " + upgradeThreeCost;
+upgradeButtonFour.textContent = "Diamant: " + upgradeFourCost;
+upgradeButtonFive.textContent = "Diamant: " + upgradeFiveCost;
+upgradeButtonSix.textContent = "Tärningar: " + upgradeSixCost;
 
 /* click event + logic */
 button.addEventListener("click", function() {
@@ -40,7 +65,7 @@ button.addEventListener("click", function() {
 	scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
 }, true);
 
-/* kod för första upgraderingen med räknare */
+/* kod för andra upgraderingen med räknare */
 upgradeButtonTwo.addEventListener("click", function() {
 	if (bank >= upgradeTwoCost) {
 		bank -= upgradeTwoCost;
@@ -62,7 +87,7 @@ upgradeButtonTwo.addEventListener("click", function() {
 	}
 }, true);
 
-// knapp och kod för andra upgraderingen
+// knapp och kod för första upgraderingen
 upgradeButtonOne.addEventListener("click", function() {
 	if (bank >= upgradeOneCost) {
 		clickValue *= 2;
@@ -108,4 +133,71 @@ upgradeButtonThree.addEventListener("click", function() {
 	}
 }, true);
 
+
+upgradeButtonFour.addEventListener("click", function() {
+	if (bank >= upgradeFourCost) {
+		clickValue *= 2;
+		bank -= upgradeFourCost;
+		upgradeFourCost *= 2;
+		grönDiamantCount += 1;
+
+		upgradeButtonFour.textContent = "Diamant: " + upgradeFourCost;
+		grönDiamantCounter.textContent = "Diamant: " + grönDiamantCount;
+		powerText.textContent = "Köpte Grön Diamant";
+		scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
+		
+		zebra = setInterval(function(){
+			bank +=2;
+			scoreText.textContent = "Points: " + bank;
+		}, 1000);
+
+	} else {
+		powerText.textContent = "Du har inte råd med en Grön Diamant";
+	}
+}, true);
+
+
+upgradeButtonFive.addEventListener("click", function() {
+	if (bank >= upgradeFiveCost) {
+		clickValue *= 2;
+		bank -= upgradeFiveCost;
+		upgradeFiveCost *= 2;
+		lilaDiamantCount += 1;
+
+		upgradeButtonFive.textContent = "Diamant: " + upgradeFiveCost;
+		lilaDiamantCounter.textContent = "Diamant: " + lilaDiamantCount;
+		powerText.textContent = "Köpte Lila Diamant";
+		scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
+		
+		zebra = setInterval(function(){
+			bank +=2;
+			scoreText.textContent = "Points: " + bank;
+		}, 1000);
+
+	} else {
+		powerText.textContent = "Du har inte råd med en Lila Diamant";
+	}
+}, true);
+
+upgradeButtonSix.addEventListener("click", function() {
+	if (bank >= upgradeSixCost) {
+		clickValue *= 2;
+		bank -= upgradeSixCost;
+		upgradeSixCost *= 2;
+		tärningCount += 1;
+
+		upgradeButtonSix.textContent = "Tärningar: " + upgradeSixCost;
+		tärningarCounter.textContent = "Tärningar: " + tärningCount;
+		powerText.textContent = "Köpte Tärningar";
+		scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
+		
+		zebra = setInterval(function(){
+			bank +=2;
+			scoreText.textContent = "Points: " + bank;
+		}, 1000);
+
+	} else {
+		powerText.textContent = "Du har inte råd med Tärningar";
+	}
+}, true);
 scoreDiv.appendChild(scoreText); // fäst p elementet i score diven.
